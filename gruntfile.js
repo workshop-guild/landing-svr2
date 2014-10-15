0f3da356f69d44da627e1bcf9319714e248cbdc9
Leon Ho <neonaleon.dev@gmail.com>
2014-10-15 16:40:17 +0800

    updated gruntfile with watch

--- gruntfile.js
+++ gruntfile.js
@@ -6,6 +6,15 @@ module.exports = function(grunt){
         src: 'api/',
         dest: 'apidoc/'
       }
+    },
+    watch: {
+      apidoc: {
+        files: ['api/**/*.js'],
+        tasks: ['apidoc'],
+        options: {
+          livereload: 1337
+        }
+      }
     }
   });
   grunt.registerTask('default', []);
